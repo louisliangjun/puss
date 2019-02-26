5b2231f02c8401a36f170e1180478ed7416ae8f8
liangjun <liangjun@aurogon.com>
2019-02-26 10:33:50 +0800

    .

--- tools/vlua_build.sh
+++ tools/vlua_build.sh
@@ -1,2 +1,6 @@
-gcc -s -O2 -pthread -Wall -Iinclude -Ipuss_toolkit -o tools/vlua tools/vlua.c puss_toolkit/*.c -llua -lm -ldl
+# vlua build: gcc -s -O2 -pthread -Wall -Iinclude -Ipuss_toolkit -o tools/vlua tools/vlua.c puss_toolkit/*.c -llua -lm -ldl
+
+SRCS=`ls 3rd/lua-5.3.5/src/*.c | grep -v lua.c | grep -v luac.c`
+#echo SRCS: $SRCS
+gcc -s -O2 -pthread -Wall -Iinclude -Ipuss_toolkit -o tools/vlua tools/vlua.c puss_toolkit/*.c $SRCS -lm -ldl
 
