08637c7fac4e4274000df54cec7c0287af0b929a
louis <louis315@126.com>
2019-03-17 10:47:18 +0800

    .

--- bin/core/app.lua
+++ bin/core/app.lua
@@ -45,13 +45,16 @@ shotcuts.register('app/reload', 'Reload scripts', 'F12', true, false, false, fal
 local function main_menu()
 	local active
 	if not imgui.BeginMenuBar() then return end
+	if imgui.BeginMenu('Window') then
+		active, show_console_window = imgui.MenuItem('Conosle', nil, show_console_window)
+		active, show_shutcut_window = imgui.MenuItem('Shutcut', nil, show_shutcut_window)
+		active, show_search_window = imgui.MenuItem('Search', nil, show_search_window)
+		imgui.EndMenu()
+	end
 	if imgui.BeginMenu('Help') then
 		active, show_imgui_demos = imgui.MenuItem('ImGUI Demos', nil, show_imgui_demos)
 		active, show_imgui_metrics = imgui.MenuItem('ImGUI Metrics', nil, show_imgui_metrics)
 		active, show_samples_window = imgui.MenuItem('Samples', nil, show_samples_window)
-		active, show_console_window = imgui.MenuItem('Conosle', nil, show_console_window)
-		active, show_shutcut_window = imgui.MenuItem('Shutcut', nil, show_shutcut_window)
-		active, show_search_window = imgui.MenuItem('Search', nil, show_search_window)
 		imgui.Separator()
 		if imgui.MenuItem('Reload', 'Ctrl+F12') then puss.reload() end
 		imgui.Separator()
@@ -163,24 +166,12 @@ local function show_main_window()
 	imgui.SetNextWindowSize(w - left_size, h - menu_size, ImGuiCond_FirstUseEver)
 	editor_window()
 
-	if show_imgui_demos then
-		show_imgui_demos = imgui.ShowDemoWindow(show_imgui_demos)
-	end
-	if show_imgui_metrics then
-		show_imgui_metrics = imgui.ShowMetricsWindow(show_imgui_metrics)
-	end
-	if show_samples_window then
-		show_samples_window = samples.update(show_samples_window)
-	end
-	if show_console_window then
-		show_console_window = console.update(show_console_window)
-	end
-	if show_shutcut_window then
-		show_shutcut_window = shotcuts.update(show_shutcut_window)
-	end
-	if show_search_window then
-		show_search_window = search.update(show_search_window)
-	end
+	show_imgui_demos = show_imgui_demos and imgui.ShowDemoWindow(true)
+	show_imgui_metrics = show_imgui_metrics and imgui.ShowMetricsWindow(true)
+	show_samples_window = show_samples_window and samples.update(true)
+	show_console_window = show_console_window and console.update(true)
+	show_shutcut_window = show_shutcut_window and shotcuts.update(true)
+	show_search_window = show_search_window and search.update(true)
 end
 
 local function do_quit_update()
@@ -240,6 +231,8 @@ __exports.init = function()
 	imgui.update(show_main_window)
 
 	filebrowser.append_folder(puss.filename_format(puss._path .. '/core', true), fs_list)
+
+	if puss.debug and (not puss.debug()) then puss.debug(true, nil, title) end
 end
 
 __exports.uninit = function()
